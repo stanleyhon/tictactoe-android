f1045577854317d8e3a3f9279ab6b13bb9df44f0
Shannon Green <shannon@shannon-green.com>
2012-07-10 01:24:37 +1000

    stuff

--- gen/com/example/R.java
+++ gen/com/example/R.java
@@ -16,7 +16,7 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
-        public static final int app_xValue=0x7f010000;
+        public static final int xValue=0x7f010000;
         /** <p>Must be an integer value, such as "<code>100</code>".
 <p>This may also be a reference to a resource (in the form
 "<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
@@ -24,7 +24,7 @@ theme attribute (in the form
 "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
 containing a value of this type.
          */
-        public static final int app_yValue=0x7f010001;
+        public static final int yValue=0x7f010001;
     }
     public static final class drawable {
         public static final int splash=0x7f020000;
@@ -60,4 +60,49 @@ containing a value of this type.
         public static final int playAI=0x7f040006;
         public static final int playHotseat=0x7f040003;
     }
+    public static final class styleable {
+        /** Attributes that can be used with a TTTButton.
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #TTTButton_xValue com.example:xValue}</code></td><td></td></tr>
+           <tr><td><code>{@link #TTTButton_yValue com.example:yValue}</code></td><td></td></tr>
+           </table>
+           @see #TTTButton_xValue
+           @see #TTTButton_yValue
+         */
+        public static final int[] TTTButton = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.example.R.attr#xValue}
+          attribute's value can be found in the {@link #TTTButton} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:xValue
+        */
+        public static final int TTTButton_xValue = 0;
+        /**
+          <p>This symbol is the offset where the {@link com.example.R.attr#yValue}
+          attribute's value can be found in the {@link #TTTButton} array.
+
+
+          <p>Must be an integer value, such as "<code>100</code>".
+<p>This may also be a reference to a resource (in the form
+"<code>@[<i>package</i>:]<i>type</i>:<i>name</i></code>") or
+theme attribute (in the form
+"<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>")
+containing a value of this type.
+          @attr name android:yValue
+        */
+        public static final int TTTButton_yValue = 1;
+    };
 }
