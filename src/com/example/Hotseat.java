f074028fa450b9c035e665cfc21f0e75c11811a0
Stanley Hon <stanleyhon348@gmail.com>
2012-07-09 18:46:49 +1000

    Added a part of the gameplay page.

--- src/com/example/Hotseat.java
+++ src/com/example/Hotseat.java
@@ -1,28 +1,21 @@
 package com.example;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 
 /**
  * Created with IntelliJ IDEA.
  * User: stanleyhon
  * Date: 9/07/12
- * Time: 4:44 PM
+ * Time: 6:21 PM
  * To change this template use File | Settings | File Templates.
  */
-public class Menu extends Activity {
+public class Hotseat extends Activity {
 
    @Override
    public void onCreate (Bundle savedInstanceState) {
       super.onCreate(savedInstanceState);
 
-      setContentView(R.layout.menu);
-   }
-
-   public void goHotseat (View view) {
-      Intent intent = new Intent(this, Hotseat.class);
-      startActivity(intent);
+      setContentView(R.layout.hotseat);
    }
 }
