8ad6e71b1005b3be19bdd8bfb1d084de7f50959c
Shannon Green <shannon@shannon-green.com>
2012-07-10 13:33:19 +1000

    fixed win detection, added draw detection, fixed random crashes

--- src/com/example/Hotseat.java
+++ src/com/example/Hotseat.java
@@ -17,7 +17,7 @@ import game.core.*;
 public class Hotseat extends Activity {
 
    private Board b = new Board();
-   private int currentPlayer = Board.CROSS;
+   private int currentPlayer = Board.NOUGHT;
    private boolean gameOver = false;
    private int turns = 0;
 
@@ -36,38 +36,41 @@ public class Hotseat extends Activity {
 
       TextView currentPlayer = (TextView) this.findViewById(R.id.currentPlayer);
 
-       if (currentPlayer.getText().toString().equals("X") && gameOver == false && this.turns != 9) {
+       if (currentPlayer.getText().toString().equals("X") && gameOver == false && this.turns < 9) {
           this.turns++;
           this.b.makeMove(xTarget, yTarget, Board.CROSS);
 
 
-          if (WinChecker.check(this.b) == true) {
+          if (WinChecker.check(this.b)) {
              TextView tv = (TextView) this.findViewById(R.id.WinIndicator);
              tv.setText("WINNER:");
              gameOver = true;
-          }
-          pressedButton.setText("O");
-          currentPlayer.setText("O");
-       } else if (currentPlayer.getText().toString().equals("O")){
-          this.b.makeMove(xTarget, yTarget, Board.NOUGHT);
-          if (WinChecker.check(this.b) == true) {
-             TextView tv = (TextView) this.findViewById(R.id.WinIndicator);
-             tv.setText("WINNER:");
-             gameOver = true;
+          } else {
+              currentPlayer.setText("O");
           }
           pressedButton.setText("X");
-          currentPlayer.setText("X");
 
-       } else if (this.turns == 9) {
+       } else if (currentPlayer.getText().toString().equals("O") && !gameOver && this.turns < 9) {
+          this.turns++;
+          this.b.makeMove(xTarget, yTarget, Board.NOUGHT);
+          if (WinChecker.check(this.b)) {
+             TextView tv = (TextView) this.findViewById(R.id.WinIndicator);
+             tv.setText("WINNER:");
+             gameOver = true;
+          } else {
+              currentPlayer.setText("X");
+          }
+          pressedButton.setText("O");
+       }
+
+
+       if (this.turns == 9 && !WinChecker.check(this.b)) {
           TextView tv = (TextView) this.findViewById(R.id.WinIndicator);
           tv.setText("TIED");
           currentPlayer.setText("");
-
        }
 
        pressedButton.setClickable(false);
-
-
 
    }
 
