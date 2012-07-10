51717b5f13f545a351dc16c64189e1cc1f842a73
Stanley Hon <stanleyhon348@gmail.com>
2012-07-10 11:55:09 +1000

    Should have tie detection

--- src/com/example/Hotseat.java
+++ src/com/example/Hotseat.java
@@ -19,6 +19,7 @@ public class Hotseat extends Activity {
    private Board b = new Board();
    private int currentPlayer = Board.CROSS;
    private boolean gameOver = false;
+   private int turns = 0;
 
    @Override
    public void onCreate (Bundle savedInstanceState) {
@@ -35,7 +36,8 @@ public class Hotseat extends Activity {
 
       TextView currentPlayer = (TextView) this.findViewById(R.id.currentPlayer);
 
-       if (currentPlayer.getText().toString().equals("X") && gameOver == false) {
+       if (currentPlayer.getText().toString().equals("X") && gameOver == false && this.turns != 9) {
+          this.turns++;
           this.b.makeMove(xTarget, yTarget, Board.CROSS);
 
 
@@ -55,6 +57,11 @@ public class Hotseat extends Activity {
           }
           pressedButton.setText("X");
           currentPlayer.setText("X");
+
+       } else if (this.turns == 9) {
+          TextView tv = (TextView) this.findViewById(R.id.WinIndicator);
+          tv.setText("TIED");
+          currentPlayer.setText("");
 
        }
 
